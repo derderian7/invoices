8070a82b05a23ae9292d95c59c92b16c7711f528
derderian7 <georgederderian7@gmail.com>
2022-06-30 16:17:42 +0300

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Database\Seeds\PermissionsTableSeeder;
+use database\seeders\PermissionTableSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PermissionsTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
     }
 }
